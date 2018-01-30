bca89802b39d5b725c36b1aebb763ea21d7f616f
quanyang <tbg127@gmail.com>
2018-01-30 11:27:45 -0600

    add

---
+++ ShoppingMangement/src/main/java/shopping/model/Product/ProductList.java
@@ -0,0 +1,41 @@
+/**
+ * 
+ */
+package shopping.model.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import shopping.bus.IProductManager;
+import shopping.bus.ProductManager;
+
+/**
+ * @author Quan Yang
+ *
+ */
+public class ProductList {
+	private List<Product> products;
+	private IProductManager manager;
+
+	public ProductList() {
+		products = new ArrayList<Product>();
+		manager = new ProductManager(this);
+	}
+
+	public List<Product> getProducts() {
+		return products;
+	}
+
+	public void setProducts(List<Product> products) {
+		this.products = products;
+	}
+	public boolean addProduct(Product product) {
+		return manager.addProduct(product);
+	}
+	public boolean removeProducts(List<Product> products) {
+		return manager.removeProducts(products);
+	}
+	public boolean updateProduct(Product product) {
+		return manager.updateProduct(product);
+	}
+}
